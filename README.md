56b15c063728a9caff5c6f45558269279127858d
James Prendergast <jamprendergast@gmail.com>
2019-01-08 14:08:57 +0000

    the addition of code for one event listener for multiple elements

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Javascript-Multiple-Event-Listener
 An Event listener for multiple javascript elements
+
+
+The following is a good test of keeping track of the answers the person selected from a multiple of 4
+it also make use of one event listener to get the specific id for a button instead of having 4 event listeners
+per button reducing the code needed to do it and it is done in javascript without any jquery
+
+https://www.kirupa.com/html5/handling_events_for_many_elements.htm 
+
+an example of the code from the link
+
+var theParent = document.querySelector("#theDude");
+theParent.addEventListener("click", doSomething, false);
+ 
+function doSomething(e) {
+    if (e.target !== e.currentTarget) {
+        var clickedItem = e.target.id;
+        alert("Hello " + clickedItem);
+    }
+    e.stopPropagation();
+}
---
+++ main.js
@@ -0,0 +1,78 @@
+var array = [0, 1, 3, 5, 10, 11, 12, 13];
+var answer = [];
+var page;
+
+window.onload = function(){
+page = 1;
+
+
+
+addNumbers(page);
+
+var theParent = document.querySelector(".container");
+theParent.addEventListener("click", nextBus, false);
+document.getElementById("next").addEventListener("click", newPage);
+document.getElementById("back").addEventListener("click", backPage);
+}
+
+
+function newPage(){
+    page = 2;
+    addNumbers(page);
+    getcolor(1);
+}
+
+function backPage(){
+    page = 1;
+    addNumbers(page);
+    getcolor(0);
+}
+
+
+
+
+
+
+function addNumbers(page){
+    if(page == 1){
+    document.getElementById("test").innerHTML = array[0];
+    document.getElementById("test2").innerHTML = array[1];
+    document.getElementById("test3").innerHTML = array[2];
+    document.getElementById("test4").innerHTML = array[3];
+    }
+    else{
+        document.getElementById("test").innerHTML = array[4];
+        document.getElementById("test2").innerHTML = array[5];
+        document.getElementById("test3").innerHTML = array[6];
+        document.getElementById("test4").innerHTML = array[7]; 
+    }
+}
+
+function getcolor(value){
+    var x = document.getElementsByTagName("button");
+    for(i=0; i<x.length; i++){
+    x[i].style.color = "white";
+    }
+    var color = answer[value][1];
+    document.getElementById(color).style.color = "red";
+}
+
+
+function nextBus(e){
+    // var list = $(this).attr("id");
+    console.log(e.target.id);
+    var value = e.target.id;
+    var answerb = document.getElementById(value).innerHTML; 
+    var x = document.getElementsByTagName("button");
+    for(i=0; i<x.length; i++){
+    x[i].style.color = "white";
+    }
+    document.getElementById(value).style.color = "red";
+    if(page == 1){
+    answer[0] = [answerb, value];
+    }
+    else{
+    answer[1] = [answerb, value];   
+    }
+    console.log(answer);
+}
